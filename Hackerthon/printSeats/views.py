8d4f9b6b45eab3049bf181a0ae2b5a9e34885966
mickeyshoes <parody9c@gmail.com>
2020-11-01 03:04:14 +0900

    안녕

--- Hackerthon/printSeats/views.py
+++ Hackerthon/printSeats/views.py
@@ -1,11 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def main(request):
-    test = {'num1':1, 'num2':1}
-    return render(request, 'index.html', test)
+
+    import serial
+
+    arduino = serial.Serial('COM4', 9600, timeout = .1) 
+    print(arduino)
+
+    i = 0
+
+    while True:
+        datas = arduino.readline()
+        if datas:
+            print(i,": ",datas)
+            i = i + 1
+            break
+
+    print(datas)
+    datas = datas.decode('utf-8')
+    print(type(datas))
+
+    #공백 문자열인 경우 제거
+    if datas:
+        data_list = datas.split('/')
+        print(data_list)
+        send_data_list = {}
+
+        for i in data_list:
+            #공백 문자열인 경우 제거
+            if i:
+                temp = i.split(':')
+                print(temp)
+                send_data_list[temp[0]] = int(temp[1])
+
+        print(send_data_list)
+
+    elif datas ==['']:
+        pass
+
+    return render(request, 'index.html', send_data_list)
 
 def introduce(request):
     return render(request, 'introduce.html')
