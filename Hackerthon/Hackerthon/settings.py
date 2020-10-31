2e663972da19d0a806b7b29169641729feec65be
mickeyshoes <parody9c@gmail.com>
2020-10-31 17:27:34 +0900

    mistake

--- Hackerthon/Hackerthon/settings.py
+++ Hackerthon/Hackerthon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'printseats.apps.printSeatsConfig',
+    'printSeats.apps.PrintseatsConfig',
 ]
 
 MIDDLEWARE = [
